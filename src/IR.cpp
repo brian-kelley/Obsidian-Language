66000fc2dfed832f7011665d0e688fe18b0ac86a
Brian Kelley <brian.honda11@gmail.com>
2018-06-29 23:55:19 -0600

    Implemented IR of for-array

--- src/IR.cpp
+++ src/IR.cpp
@@ -82,12 +82,16 @@ namespace IR
     auto savedBreak = breakLabel;
     auto savedCont = continueLabel;
     Label* top = new Label;     //top: just before condition eval
-    Label* bottom = new Label;  //bottom: just after loop (exit)
+    Label* mid = new Label;     //mid: just before incr (continue)
+    Label* bottom = new Label;  //bottom: just after loop (break)
+    breakLabel = bottom;
+    continueLabel = mid;
     stmts.push_back(top);
     //here the condition is evaluated
     //false: jump to bottom, otherwise continue
     stmts.push_back(new CondJump(fc->cond, bottom));
     addStatement(fc->inner);
+    stmts.push_back(mid);
     addStatement(fc->increment);
     stmts.push_back(new Jump(top));
     stmts.push_back(bottom);
@@ -109,12 +113,14 @@ namespace IR
     stmts.push_back(new AssignIR(counterExpr, fr->begin));
     auto savedBreak = breakLabel;
     auto savedCont = continueLabel;
-    Label* top = new Label;     //top: just before condition eval
-    Label* bottom = new Label;  //bottom: just after loop (exit)
+    Label* top = new Label;     //just before condition eval
+    Label* mid = new Label;     //before increment (continue)
+    Label* bottom = new Label;  //just after loop (break)
     stmts.push_back(top);
     //condition eval and possible break
     stmts.push_back(new CondJump(cond, bottom));
     addStatement(fr->inner);
+    stmts.push_back(mid);
     addStatement(new AssignIR(counterExpr, counterP1));
     stmts.push_back(new Jump(top));
     stmts.push_back(bottom);
@@ -125,6 +131,76 @@ namespace IR
 
   void SubroutineIR::addForArray(ForArray* fa)
   {
-    //generate a standard 
+    //generate a standard 0->n loop for each dimension
+    //break corresponds to jump from outermost loop,
+    //but continue just jumps to innermost increment
+    vector<Label*> topLabels;
+    vector<Label*> midLabels;
+    vector<Label*> bottomLabels;
+    //how many loops/counters there are
+    int n = fa->counters.size();
+    for(int i = 0; i < n; i++)
+    {
+      topLabels.push_back(new Label);
+      midLabels.push_back(new Label);
+      bottomLabels.push_back(new Label);
+    }
+    //subarrays: previous subarray indexed with loop counter
+    //array[i] is the array traversed in loop of depth i
+    vector<Expression*> subArrays(n, nullptr);
+    Expression* zeroLong = new Converted(new IntLiteral(0), primitives[Prim::LONG]);
+    zeroLong->finalResolve();
+    Expression* oneLong = new Converted(new IntLiteral(1), primitives[Prim::LONG]);
+    oneLong->finalResolve();
+    subArrays[0] = fa->arr;
+    for(int i = 1; i < n; i++)
+    {
+      subArray[i] = new Indexed(subArray[i - 1], new VarExpr(fa->counters[i]));
+      subArray[i]->finalResolve();
+    }
+    vector<Expression*> dims(n, nullptr);
+    for(int i = 0; i < n; i++)
+    {
+      dims[i] = new ArrayLength(subArrays[i]);
+      dims[i]->finalResolve();
+    }
+    for(int i = 0; i < n; i++)
+    {
+      //initialize loop i counter with 0
+      //convert "int 0" to counter type if needed
+      stmts.push_back(new AssignIR(new VarExpr(fa->counters[i])), zeroLong);
+      stmts.push_back(topLabels[i]);
+      //compare against array dim: if false, break from loop i
+      Expression* cond = new BinaryArith(fa->counters[i], CMPL, dims[i]);
+      cond->finalResolve();
+      stmts.push_back(new CondJump(cond, bottomLabels[i]));
+    }
+    //update iteration variable before executing inner body
+    VarExpr* iterVar = new VarExpr(fa->iter);
+    iterVar->finalResolve();
+    Indexed* iterValue = new Indexed(subArrays.back(), new VarExpr(fa->counters.back()));
+    iterValue->finalResolve();
+    stmts.push_back(new AssignIR(iterVar, iterValue));
+    //add user body
+    //user break stmts break from whole ForArray
+    //user continue just goes to top of innermost
+    Label* savedBreak = breakLabel;
+    Label* savedCont = continueLabel;
+    breakLabel = bottomLabels.front();
+    continueLabel = midLabels.back();
+    addStatement(fa->inner);
+    breakLabel = savedBreak;
+    continueLabel = savedCont;
+    //in reverse order, add the loop increment/closing
+    for(int i = n - 1; i >= 0; i--)
+    {
+      stmts.push_back(midLabels[i]);
+      Expression* counterIncremented = new BinaryArith(iterVar, PLUS, oneLong);
+      counterIncremented->finalResolve();
+      stmts.push_back(new AssignIR(new VarExpr(fa->counters[i], counterIncremented)));
+      stmts.push_back(new Jump(topLabels[i]));
+      stmts.push_back(bottomLabels[i]);
+    }
   }
 }
+
