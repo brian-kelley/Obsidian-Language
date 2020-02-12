2ba4b0c8e8c356ba0863e4998c510c636542e0e1
Brian Kelley <brian.honda11@gmail.com>
2020-02-11 22:08:50 -0700

    Increased lex fuzz input size, but reduced #iters
    Shouldn't ever hit process limit on any system now

--- test/LexerFuzzing.cpp
+++ test/LexerFuzzing.cpp
@@ -23,14 +23,14 @@ int main(int argc, const char** argv)
 {
   seedRNG();
   INTERNAL_ASSERT(argc == 2);
-  size_t len = 128 * 1024;
+  size_t len = 512 * 1024;
   char* text = new char[len + 1];
   text[len] = 0;
   bool doAll = !strcmp(argv[1], "--all");
   //Iterations can't be too high because even though
   //the compiler processes aren't running concurrently,
   //every fork() counts against the OS process limit
-  for(int iter = 0; iter < 64; iter++)
+  for(int iter = 0; iter < 8; iter++)
   {
     for(size_t i = 0; i < len; i++)
     {
